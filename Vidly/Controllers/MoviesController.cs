d8522f0dcf7fe4212000fe906b0189f83e5b084b
mishrakeshav <47028747+mishrakeshav@users.noreply.github.com>
2021-07-18 18:50:01 +0530

    Section 3 Working with data : Exercise

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -5,14 +5,41 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
 
+        private ApplicationDbContext _context;
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ViewResult Index()
+        {
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
+
+            return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movies = _context.Movies.Include(m => m.Genre).SingleOrDefault(c => c.Id == id);
+
+            if (movies == null)
+                return HttpNotFound();
+
+            return View(movies);
+        }
+
         // GET: Movies/Random
-        //test push for user name
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
@@ -30,33 +57,6 @@ namespace Vidly.Controllers
 
             return View(viewModel);
         }
-
-        public ViewResult Index()
-        {
-            var movies = GetMovies();
-
-            return View(movies);
-        }
-
-        public ActionResult Details(int id)
-        {
-            var movies = GetMovies().SingleOrDefault(c => c.Id == id);
-
-            if (movies == null)
-                return HttpNotFound();
-
-            return View(movies);
-        }
-
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Avengers" },
-                new Movie { Id = 2, Name = "Shrek" }
-            };
-        }
-
 
     }
 }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace Vidly.Models
 {
+    //movies/random
     public class Movie
     {
         public int Id { get; set; }
         public string Name { get; set; }
 
-        //movies/random
+        [Required]
+        public Genre Genre { get; set; }
+        public byte GenreId { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public byte NumberInStock { get; set; }
+        
     }
 }
